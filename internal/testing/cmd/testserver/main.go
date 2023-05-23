62ad03a018cf549d5dc46faac5c64a4f79271799
Lai Tuan Anh <laituananh1711@gmail.com>
2023-05-23 20:04:11 +0900

    update the test server

--- internal/testing/cmd/testserver/main.go
+++ internal/testing/cmd/testserver/main.go
@@ -9,9 +9,9 @@ import (
 	"google.golang.org/grpc"
 	grpcreflect "google.golang.org/grpc/reflection"
 
-	barpb "github.com/moneyforward/grpcake/internal/testing/pb/bar"
-	bazpb "github.com/moneyforward/grpcake/internal/testing/pb/baz"
-	foopb "github.com/moneyforward/grpcake/internal/testing/pb/foo"
+	advancedpb "github.com/moneyforward/grpcake/internal/testing/pb/advanced"
+	basicpb "github.com/moneyforward/grpcake/internal/testing/pb/basic"
+	compositepb "github.com/moneyforward/grpcake/internal/testing/pb/composite"
 )
 
 func main() {
@@ -21,10 +21,12 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
-	foopb.RegisterExampleServiceServer(grpcServer, newFooServer())
-	barpb.RegisterTestServiceServer(grpcServer, newBarServer())
-	bazpb.RegisterTestServiceServer(grpcServer, newBazServer())
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor((&loggingInterceptor{}).UnaryServerInterceptor),
+	)
+	basicpb.RegisterBasicServiceServer(grpcServer, newBasicServer())
+	advancedpb.RegisterAdvancedServiceServer(grpcServer, newAdvancedServer())
+	compositepb.RegisterCompositeServiceServer(grpcServer, newCompositeServer())
 
 	// Register reflection service on gRPC server.
 	grpcreflect.Register(grpcServer)
@@ -36,38 +38,52 @@ func main() {
 	}
 }
 
-type fooServer struct {
-	*foopb.UnimplementedExampleServiceServer
+type basicServer struct {
+	*basicpb.UnimplementedBasicServiceServer
 }
 
-func newFooServer() *fooServer {
-	return &fooServer{}
+func newBasicServer() *basicServer {
+	return &basicServer{}
 }
 
-func (e *fooServer) UnaryExample(ctx context.Context, pb *foopb.BasicTypes) (*foopb.BasicTypes, error) {
+func (e *basicServer) UnaryExample(ctx context.Context, pb *basicpb.BasicType) (*basicpb.BasicType, error) {
 	return pb, nil
 }
 
-type barServer struct {
-	*barpb.UnimplementedTestServiceServer
+type advancedServer struct {
+	*advancedpb.UnimplementedAdvancedServiceServer
 }
 
-func newBarServer() *barServer {
-	return &barServer{}
+func newAdvancedServer() *advancedServer {
+	return &advancedServer{}
 }
 
-func (b *barServer) UnaryExample(ctx context.Context, pb *barpb.ExampleMessage) (*barpb.ExampleMessage, error) {
+func (b *advancedServer) UnaryExample(ctx context.Context, pb *advancedpb.AdvancedType) (*advancedpb.AdvancedType, error) {
 	return pb, nil
 }
 
-type bazServer struct {
-	*bazpb.UnimplementedTestServiceServer
+type compositeServer struct {
+	*compositepb.UnimplementedCompositeServiceServer
 }
 
-func newBazServer() *bazServer {
-	return &bazServer{}
+func newCompositeServer() *compositeServer {
+	return &compositeServer{}
 }
 
-func (b *bazServer) Greeting(ctx context.Context, pb *bazpb.GreetingMessage) (*bazpb.GreetingMessage, error) {
+func (b *compositeServer) Greeting(ctx context.Context, pb *compositepb.CompositeType) (*compositepb.CompositeType, error) {
 	return pb, nil
+}
+
+type loggingInterceptor struct{}
+
+func (i *loggingInterceptor) UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// Log the method name
+	log.Printf("gRPC method called: %s", info.FullMethod)
+
+	// Log the request headers
+	// md, _ := metadata.FromIncomingContext(ctx)
+	// log.Printf("Request headers: %v", md)
+
+	// Call the next handler in the chain
+	return handler(ctx, req)
 }
